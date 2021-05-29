4eb83af1d30fce2bffc1a9c2ce965eaeb7c30e10
huyitviet <huyitviet@gmail.com>
2021-05-30 06:16:48 +0700

    update v2

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import {View, Text, } from 'react-native'
+import { View, Text, } from 'react-native'
 
 import firebase from 'firebase'
 
@@ -31,25 +31,27 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
 
 const Stack = createStackNavigator();
 
 
 export class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       loaded: false,
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
-      if(!user){
+      console.log(user)
+      if (!user) {
         this.setState({
           loggedIn: false,
           loaded: true,
         })
-      }else {
+      } else {
         this.setState({
           loggedIn: true,
           loaded: true,
@@ -59,32 +61,38 @@ export class App extends Component {
   }
 
   render() {
-    const {loggedIn, loaded } = this.state
-    if(!loaded){
-      return(
-        <View style={{ flex: 1, justifyContent: 'center'}}> 
+    const { loggedIn, loaded } = this.state
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
           <Text>Loading</Text>
         </View>
       )
     }
-    if(!loggedIn) {
+    if (!loggedIn) {
       return (
         <NavigationContainer>
-        <Stack.Navigator initialRouteNam="Landing">
-          <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Register" component={RegisterScreen}  />
-        </Stack.Navigator>
-      </NavigationContainer>
+          <Stack.Navigator initialRouteNam="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       )
     }
 
-    return(
+    return (
       <Provider store={store}>
-        <MainScreen/> 
+        <NavigationContainer>
+          <Stack.Navigator initialRouteNam="Main">
+            <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Add" component={AddScreen} />
+
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
-      
-      )
-    }
+
+    )
+  }
 }
 
 export default App
